93bec369d2481725484d4db3c3a40800c7d45314
GMBN <gianboschette@gmail.com>
2019-01-12 18:48:45 -0200

    alter widget

--- app/Controller/Widget.php
+++ app/Controller/Widget.php
@@ -10,7 +10,7 @@ class Widget {
         $name = isset($_GET['name']) ? $_GET['name'] : false;
         $base = isset($_GET['base']) ? $_GET['base'] : 'bitcoin';
         $moeda = isset($_GET['moeda']) ? $_GET['moeda'] : 'USD';
-        $this->saveRefer();
+//        $this->saveRefer();
 
         if ($name) {
             $data = (new \Model\Moeda())->findOne($name, $moeda);
@@ -27,19 +27,19 @@ class Widget {
         return;
     }
 
-    function saveRefer() {
-
-        $url_refer = isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : null;
-        $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-        $ip = $_SERVER['REMOTE_ADDR'];
-
-
-        $obj = new \Model\UrlReferrer();
-        $obj->setTag('widget');
-        $obj->setUrlRefer($url_refer);
-        $obj->setUrlDestino($actual_link);
-        $obj->setIp($ip);
-        $obj->insert();
-    }
+//    function saveRefer() {
+//
+//        $url_refer = isset($_SERVER["HTTP_REFERER"]) ? $_SERVER["HTTP_REFERER"] : null;
+//        $actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+//        $ip = $_SERVER['REMOTE_ADDR'];
+//
+//
+//        $obj = new \Model\UrlReferrer();
+//        $obj->setTag('widget');
+//        $obj->setUrlRefer($url_refer);
+//        $obj->setUrlDestino($actual_link);
+//        $obj->setIp($ip);
+//        $obj->insert();
+//    }
 
 }
