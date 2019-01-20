c82baafbbe251ffada2a14358e405f6486dbedab
GMBN <gianboschette@gmail.com>
2019-01-20 14:44:29 -0200

    Client API adding call with pagination

--- app/Controller/CronCoin.php
+++ app/Controller/CronCoin.php
@@ -2,11 +2,9 @@
 
 namespace Controller;
 
-class CronCoin
-{
+class CronCoin {
 
-    function getGlobalMarket($moeda)
-    {
+    function getGlobalMarket($moeda) {
         $url = "https://api.coingecko.com/api/v3/global";
         $json = file_get_contents($url);
         $data = json_decode($json, 1);
@@ -14,8 +12,7 @@ class CronCoin
         return $data['data']['total_market_cap'][$moeda];
     }
 
-    function insert()
-    {
+    function insert() {
 
 
         $db = \Base\DB::connect();
@@ -33,40 +30,48 @@ class CronCoin
 
                 $marketGlobal_dinamico = $this->getGlobalMarket($moedaLower);
 
-                $url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" . $moedaLower . "";
-                $json = file_get_contents($url);
-                $data = json_decode($json, 1);
+                $page = 1;
+                $countResults = 1;
 
-                foreach ($data as $d) {
+                while ($countResults > 0) {
+                    $url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=" . $moedaLower . "&per_page=250&page=" . $page;
+                    $json = file_get_contents($url);
+                    $data = json_decode($json, 1);
 
-                    //calcula o percentual de dominancia
-                    $moeda_market_cap_dinamico = (float)$d['market_cap'];
-                    $dominance = $moeda_market_cap_dinamico * 100 / $marketGlobal_dinamico;
-                    
-                    $athDate = explode('T',$d['ath_date'])[0];
+                    foreach ($data as $d) {
 
-                    $model = new \Model\Moeda($db);
-                    $model->setCodigo($d['id']);
-                    $model->setMoeda($moeda);
-                    $model->setMoedaChar($char);
-                    $model->setRank($d['market_cap_rank']);
-                    $model->setName($d['name']);
-                    $model->setSymbol($d['symbol']);
-                    $model->setPriceMoeda((float)$d['current_price']);
-                    $model->setVolume24hMoeda($d['total_volume']);
-                    $model->setAvailableSupply($d['circulating_supply']);
-                    $model->setTotalSupply($d['circulating_supply']);
-                    $model->setMaxSupply($d['total_supply']);
-                    $model->setMarketCapMoeda($d['market_cap']);
-                    $model->setPercentDominance($dominance);
-                    $model->setPercentChange24h($d['price_change_percentage_24h']);
-                    $model->setAth($d['ath']);
-                    $model->setAthDate($athDate);
-                    $model->insert();
-                    
-                    $this->saveImage($d['image'],$d['id']);
+                        //calcula o percentual de dominancia
+                        $moeda_market_cap_dinamico = (float) $d['market_cap'];
+                        $dominance = $moeda_market_cap_dinamico * 100 / $marketGlobal_dinamico;
 
-                    echo "inserted: " . $d['symbol'] . '-' . $moeda . "\n";
+                        $athDate = explode('T', $d['ath_date'])[0];
+
+                        $model = new \Model\Moeda($db);
+                        $model->setCodigo($d['id']);
+                        $model->setMoeda($moeda);
+                        $model->setMoedaChar($char);
+                        $model->setRank($d['market_cap_rank']);
+                        $model->setName($d['name']);
+                        $model->setSymbol($d['symbol']);
+                        $model->setPriceMoeda((float) $d['current_price']);
+                        $model->setVolume24hMoeda($d['total_volume']);
+                        $model->setAvailableSupply($d['circulating_supply']);
+                        $model->setTotalSupply($d['circulating_supply']);
+                        $model->setMaxSupply($d['total_supply']);
+                        $model->setMarketCapMoeda($d['market_cap']);
+                        $model->setPercentDominance($dominance);
+                        $model->setPercentChange24h($d['price_change_percentage_24h']);
+                        $model->setAth($d['ath']);
+                        $model->setAthDate($athDate);
+                        $model->insert();
+
+                        $this->saveImage($d['image'], $d['id']);
+
+                        echo "page " . $page . " inserted: " . $d['symbol'] . '-' . $moeda . "\n";
+                    }
+
+                    $countResults = count($data);
+                    $page++;
                 }
             }
             $db->commit();
@@ -80,27 +85,24 @@ class CronCoin
         }
     }
 
-    function deleteAll($db)
-    {
+    function deleteAll($db) {
         $model = new \Model\Moeda($db);
         $model->deleteAll();
     }
 
-    private function saveImage($url,$code)
-    {
+    private function saveImage($url, $code) {
 
-            $fileName = ROOT . '/public/assets/img/coin/' . $code. '.png';
+        $fileName = ROOT . '/public/assets/img/coin/' . $code . '.png';
 
-            if (!file_exists($fileName) || filesize($fileName) < 10) {
-                
-                echo "image saved: ".$code.PHP_EOL;
-                $image = file_get_contents($url);
-                file_put_contents(ROOT . '/public/assets/img/coin/' . $code. '.png', $image);
-            }
+        if (!file_exists($fileName) || filesize($fileName) < 10) {
+
+            echo "image saved: " . $code . PHP_EOL;
+            $image = file_get_contents($url);
+            file_put_contents(ROOT . '/public/assets/img/coin/' . $code . '.png', $image);
         }
-    
-    function listAll()
-    {
+    }
+
+    function listAll() {
         $data = (new \Model\Moeda())->findAllName();
 
         $retorno = [];
