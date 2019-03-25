247e1892af000ba6359648e5674a5a85d2124a91
GMBN <gianboschette@gmail.com>
2019-03-24 22:56:52 -0300

    changing layout

--- app/View/coinmaxprice/data.php
+++ app/View/coinmaxprice/data.php
@@ -29,7 +29,7 @@ $inputFavorite = isset($_GET['favorite']) ? $_GET['favorite'] : false;
 <input type="hidden" id="order_name" value="<?= $inputOrderName ?>"/>
 <input type="hidden" id="order_type" value="<?= $inputOrderType ?>"/>
 <input type="hidden" id="compare_coin" value="<?= $compare_coin ?>"/>
-<div class="row" style="margin: 0px;">
+<div class="row" style="margin: 5px;">
     <div class="col-md-12">
         <h1 style="padding: 3px;margin-top:20px;font-size:25px"><?= _e($_title) ?>
             <i data-toggle="tooltip" title="" class="fa fa-question-circle-o help" id="btn_help_max_price" style="color:#9124a3;font-size: 30px" data-original-title="<?= _e('Ajuda') ?>"></i>
